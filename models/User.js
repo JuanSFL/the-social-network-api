85a878a11106b3008e18df4dc01ed6e2408c86ae
JuanSFL <molina98j@yahoo.com>
2023-02-09 17:24:14 -0500

    added user schema to User.js and added a virtual called 'friendCount' to return how many friends a user has.

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model, Types } = require('mongoose');
+const { Thought } = require('.');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        // Regex that checks if user input is a valid email address.
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    //array of id's referencing the thought model
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought"
+    },
+    // array of 'friends' id's referencing the user model
+    ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true,
+    }
+}
+);
+// virtual that gets the friendcount of the user
+userSchema.virtual("friendCount").get(()=> {
+    return this.friends.length+1;
+  });
+  // creates User model using the UserSchema
+  const User = model('User', userSchema);
+  // export User model
+  module.exports = User;
